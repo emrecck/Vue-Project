3fb6ab3370ca44667e43043f8fee104f2df93e2d
EMRE <1907burem@gmail.com>
2020-11-07 17:46:14 +0300

    product fixed

--- intro/src/components/Product.vue
+++ intro/src/components/Product.vue
@@ -1,73 +1,67 @@
 <template>
-  <body>
-    <div class="product-main">
-      <div class="product-bilgileri">
+  <div class="container">
+    <div class="product-bilgi col-sm-12 row">
+      <div class="product-image col-lg-6 col-sm-12 pb-sm-5">
+        <a href=""
+          ><img
+            class="productimg"
+            src="../assets/images/urun/0001149_citir-biber-aci-.jpeg"
+            alt=""
+        /></a>
+      </div>
 
-        <div class="product-bilgi">
-          <div class="product-image">
-            <a href=""
-              ><img
-                src="../assets/images/urun/0001149_citir-biber-aci-.jpeg"
+      <div class="baslik-sepet-ozellik col-lg-6 col-sm-12">
+        <div class="product-baslik text-left clearfix">
+          <h1 class="d-inline-block">Çıtır Biber ( Acı )</h1>
+          <span class="float-right">22,90 TL</span>
+          <p>1 Kg Acı Biber</p>
+        </div>
+
+        <div class="product-sepet text-left col-sm-12 px-0">
+          <span class="product-quantity">
+            <label class="adet-label mt-4 pt-4">Adet:</label>
+
+            <div class="buttons pb-5 mb-4">
+              <div class="btn-group" role="group" aria-lael="Basic example">
+                <button type="button" class="minus-button" title="-"></button>
+                <input type="text" class="input-button" value="1" />
+                <button type="button" class="plus-button" title="+"></button>
+              </div>
+            </div>
+
+            <input
+              type="button"
+              class="product-buton col-sm-12 col-lg-7 mb-5"
+            />
+
+            <div class="product-quality">
+              <img
+                src="../assets/images/urun/product_quality_icons.png"
                 alt=""
-            /></a>
-          </div>
-
-          <div class="baslik-sepet-ozellik">
-            <div class="product-baslik">
-              <h1>Çıtır Biber ( Acı )</h1>
-              <span>22,90 TL</span>
-              <p>1 Kg Acı Biber</p>
+              />
             </div>
+            <img
+              class="kargo-bedava-badge"
+              src="../assets/images/urun/kargo_bedava_badge.png"
+            />
+          </span>
+        </div>
 
-            <div class="product-sepet">
-              <div>
-                <p>Adet:</p>
-                <div class="spinner">
-                  <a href=""
-                    ><img src="../assets/images/urun/spinner_minus.png" alt=""
-                  /></a>
-                  <img src="../assets/images/urun/spinner_bg.png" alt="" />
-                  <a href=""
-                    ><img src="../assets/images/urun/spinner_plus.png" alt=""
-                  /></a>
-                </div>
-                <input type="button" class="product-buton" />
-                <div class="product-quality">
-                  <img
-                    src="../assets/images/urun/product_quality_icons.png"
-                    alt=""
-                  />
-                </div>
-              </div>
-
-              <div class="kargo-img">
-                <div class="kargo-bedava">
-                  <img src="../assets/images/urun/kargo_bedava_badge.png" />
-                </div>
-              </div>
-            </div>
-
-            <div class="product-ozellik">
-              <h3>
-                <img src="../assets/images/urun/bugday_icon.png" alt="" /> Ürün
-                Özellikleri
-              </h3>
-              <p>
-                Taze, çıtır çıtır biberlerimizi günlük olarak hasat ediyor;
-                siparişinizle topluyoruz.<br />
-                Yemeklerinizde, kahvaltılarınızda afiyetle yiyebilirsiniz.
-              </p>
-              <a href="https://www.hasanbey.com/tr/gunluk-cikanlar"
-                ><img
-                  src="../assets/images/urun/web-sk-15-yeni2-66-.png"
-                  alt=""
-              /></a>
-            </div>
-          </div>
+        <div class="product-ozellik text-left mt-5 col-sm-12 px-0">
+          <h3>
+            <img src="../assets/images/urun/bugday_icon.png" alt="" /> Ürün
+            Özellikleri
+          </h3>
+          <p>
+            Taze, çıtır çıtır biberlerimizi günlük olarak hasat ediyor;
+            siparişinizle topluyoruz.
+            Yemeklerinizde, kahvaltılarınızda afiyetle yiyebilirsiniz.
+          </p>
+          <a href="https://www.hasanbey.com/tr/gunluk-cikanlar"></a>
         </div>
       </div>
     </div>
-  </body>
+  </div>
 </template>
 
 <script>
@@ -78,198 +72,113 @@ export default {
 </script>
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-.product-main {
-  height: 800px;
-  display: grid;
-  grid-template-rows: 1fr 1fr;
-  margin-left: 130px;
-  margin-right: 130px;
+.product-image {
+  max-width: 100%;
+  width: 100%;
 }
-
-.product-bilgi {
-  display: grid;
-  grid-template-columns: 1fr 1fr;
+.productimg {
+  max-width: 100%;
 }
-
-.baslik-sepet-ozellik {
-  display: grid;
-  grid-template-rows: 1fr 3fr 2fr;
-}
-
-body {
-  font-family: kg_turning_tablesregular, sans-serif;
-}
-
-.top {
-  display: grid;
-  grid-template-columns: 1fr 1fr;
-}
-
-.top strong {
-  color: #e43743;
-  font-weight: bold;
-  font-size: 14px;
-}
-
-.top a {
-  float: left;
-  margin-right: 10px;
-  color: #b76f3b;
-  font-size: 14px;
-  font-weight: bold;
-  text-decoration: none;
-}
-
-.listeye-don a {
-  text-decoration: none;
-  background-color: transparent;
-  cursor: pointer;
-}
-
-.listeye-don input {
-  margin-left: 500px;
-  cursor: pointer;
-  background: url(../assets/images/urun/listeye_don_btn.png) no-repeat;
-  text-indent: -9999px;
-  border: 0px;
-  width: 111px;
-  height: 35px;
-}
-
-.listeye-don input:hover {
-  margin-left: 500px;
-  cursor: pointer;
-  background: url(../assets/images/urun/listeye_don_btn_hover.png) no-repeat;
-  text-indent: -9999px;
-  border: 0px;
-  width: 111px;
-  height: 35px;
-}
-
-.product-image img {
-  margin-top: 30px;
-  width: 500px;
-}
-
 .product-baslik h1 {
-  margin-left: -100px;
-  font-family: inherit;
   font-weight: bold;
-  width: 79%;
   line-height: initial;
-  float: left;
-  display: initial;
-  font-size: 24px;
+  font-size: 2.4rem;
 }
-
 .product-baslik span {
-  margin-left: 400px;
-  margin-top: -40px;
   color: #d53235;
   font-family: inherit;
   font-weight: bold;
-  width: 79%;
   line-height: initial;
-  float: left;
   display: initial;
-  font-size: 24px;
+  font-size: 2.4rem;
 }
-
 .product-baslik p {
-  margin-left: -100px;
-  margin-top: 60px;
   color: #737373;
-  font-family: kg_turning_tablesregular, sans-serif;
   font-weight: bold;
+  font-size: 1.6rem;
 }
-
 .product-sepet {
-  display: grid;
-  grid-template-rows: 2fr 1fr;
+  position: relative;
 }
-
-.product-sepet p {
-  font-size: 13px;
+.adet-label {
+  font-size: 1.3rem;
   display: block;
   color: #b76f3b;
   text-align: left;
   font-weight: bold;
-  margin-top: 10px;
-  margin-left: 80px;
+  margin: 5px 0;
 }
-
-.spinner {
-  margin-left: -345px;
+.minus-button {
+  background-image: url("../assets/images/urun/spinner_minus.png");
+  background-clip: border-box;
+  background-size: cover;
+  border: 0;
+  color: #b76f3b;
+  width: 45px;
+  height: 41px;
+  font-size: 1.4rem;
+  background-color: transparent;
 }
-
-.spinner img {
-  margin-top: -10px;
-  margin-left: -3px;
+.input-button {
+  text-align: center;
+  background-image: url("../assets/images/urun/spinner_bg.png");
+  background-clip: border-box;
+  background-size: cover;
+  text-align: center;
+  border: 0;
+  color: #b76f3b;
+  width: 45px;
+  height: 41px;
+  font-size: 1.4rem;
+  background-color: transparent;
 }
-
+.plus-button {
+  background-image: url("../assets/images/urun/spinner_plus.png");
+  background-clip: border-box;
+  background-size: cover;
+  text-align: center;
+  border: 0;
+  color: #b76f3b;
+  width: 45px;
+  height: 41px;
+  font-size: 1.4rem;
+  background-color: transparent;
+}
 .product-buton {
   cursor: pointer;
   border-radius: 0;
-  margin-top: 25px;
-  margin-left: -200px;
   background: url(../assets/images/urun/sepete_ekle_btn.png) center center
     no-repeat !important;
   background-color: #74a02f !important;
-  width: 279px;
   height: 46px;
   border: 0;
   color: transparent;
 }
-
 .product-buton:hover {
-  cursor: pointer;
-  border-radius: 0;
-  margin-top: 25px;
-  margin-left: -90px;
-  background: url(../assets/images/urun/sepete_ekle_btn.png) center center
-    no-repeat !important;
   background-color: rgb(99, 128, 42) !important;
-  width: 279px;
-  height: 46px;
-  border: 0;
-  color: transparent;
+  transition: background-color 0.4s ease-in-out 0s;
 }
-
 .product-quality img {
-  margin-top: 20px;
-  margin-left: -95px;
   width: 350px;
 }
-
-.kargo-img {
-  margin-top: -90px;
+.kargo-bedava-badge{
+  position: absolute;
+  top: 0;
+  width: 125px;
+  margin: 0;
+  float: right;
+  right: 0;
+  border: 0;
 }
-
-.kargo-bedava img {
-  width: 140px;
-  margin-top: -120px;
-  margin-left: 495px;
-}
-
 .product-ozellik h3 {
-  margin-top: -90px;
-  margin-left: 55px;
   color: rgb(183, 111, 59);
   font-family: kg_turning_tablesregular, sans-serif;
   text-align: left;
   font-size: 18px;
 }
-
 .product-ozellik p {
   font-size: 14px;
-  margin-top: -5px;
-  margin-left: -60px;
   color: rgb(136, 111, 93);
   font-family: Georgia, "Times New Roman", Times, serif;
-}
-
-.product-ozellik a img {
-  margin-top: 15px;
-  margin-left: -60px;
 }
 </style>
